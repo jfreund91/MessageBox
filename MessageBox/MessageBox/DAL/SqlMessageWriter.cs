55a7b710729246b5fa3c3bf6b7f6341786dd499b
Jake <jfreund91@gmail.com>
2019-06-22 21:49:36 -0400

    Add Read/Write for Messages to DB

---
+++ MessageBox/MessageBox/DAL/SqlMessageWriter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using MessageBox.Models;
+
+namespace MessageBox.DAL
+{
+    public class SqlMessageWriter : IMessageWriter
+    {
+        /// <summary>
+        /// The connection string.
+        /// </summary>
+        private string cn;
+
+        /// <summary>
+        /// Creates an IMessageWriter that can write to a SQL database.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public SqlMessageWriter(string connectionString)
+        {
+            this.cn = connectionString;
+        }
+
+        /// <summary>
+        /// Saves a message to the database.
+        /// </summary>
+        public void SaveMessage(Message message)
+        {
+            string sql = "INSERT INTO [messages] VALUES(@topicId, @entry_date, @content, @is_deleted);";
+            using (SqlConnection conn = new SqlConnection(this.cn))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@topicId", message.TopicId);
+                cmd.Parameters.AddWithValue("@entry_date", DateTime.UtcNow);
+                cmd.Parameters.AddWithValue("@content", message.MessageContent);
+                cmd.Parameters.AddWithValue("@is_deleted", 0);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Gets the messages for a specific topic.
+        /// </summary>
+        /// <param name="topicId">Topic ID</param>
+        /// <returns>A List of Messages for the given Topic ID.</returns>
+        public IList<Message> GetMessages(int topicId)
+        {
+            List<Message> messages = new List<Message>();
+            string sql = "SELECT TOP (1000) * FROM [messages] WHERE topicId = @topicId;";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.cn))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@topicId", topicId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        messages.Add(ConvertReaderToMessage(reader));
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Swollow the exception for now.
+                throw;
+            }
+
+            return messages;
+        }
+
+        /// <summary>
+        /// Maps a Reader Object from the SQLDataReader to a Message Object.
+        /// </summary>
+        /// <param name="reader">reader</param>
+        /// <returns>Message</returns>
+        private Message ConvertReaderToMessage(SqlDataReader reader)
+        {
+            Message msg = new Message();
+            msg.ID = Convert.ToInt32(reader["id"]);
+            msg.MessageContent = Convert.ToString(reader["content"]);
+            msg.PostDate = Convert.ToDateTime(reader["entry_date"]);
+            msg.TopicId = Convert.ToInt32(reader["topicId"]);
+
+            return msg;
+        }
+    }
+}
---
+++ MessageBox/MessageBox/Models/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBox.Models
+{
+    public class Message
+    {
+        /// <summary>
+        /// The message id.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The Topic ID the message belongs in.
+        /// </summary>
+        public int TopicId { get; set; }
+
+        /// <summary>
+        /// The content of the message.
+        /// </summary>
+        public string MessageContent { get; set; }
+
+        /// <summary>
+        /// The date the post was made.
+        /// </summary>
+        public DateTime PostDate { get { return this._postDate; } }
+
+    }
+}
